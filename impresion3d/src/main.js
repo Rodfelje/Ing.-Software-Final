7e760dba92b0df15905c322d9b8e561eb7a42637
Rodfelje <rodfelje1998@hotmail.com>
2018-12-12 23:23:51 -0500

    Prototipo de la Pagina Principal implementado > Falta hacer "Nuestros Servicios"

--- impresion3d/src/main.js
+++ impresion3d/src/main.js
@@ -6,3 +6,20 @@ import "bootstrap-vue/dist/bootstrap-vue.css"
 
 Vue.use(BootstrapVue)
 Vue.use(Vuex)
+
+var navMainPageVue = new Vue({
+    el:"#NavMainPage",
+    data:{
+        logged: false,
+        logUserMsg: "Log In",
+        logUserLink: "./client/login.html",
+        logCartLink: "./client/login.html"
+    }
+})
+
+var mainPageVue = new Vue({
+    el:"#MainPage",
+    data:{
+        popularName1: "Batman" 
+    }
+})
